6852e08c434bba145f69c7d879f22a53c8310028
Darian Anthony Patrick <dpatrick@wikimedia.org>
2016-07-15 11:26:19 -0700

    Initial commit, based on https://github.com/pigulla/nsp-formatter-checkstyle

---
+++ README.md
@@ -0,0 +1,5 @@
+# nsp-formatter-remarkup
+
+> Summary-style formatter for the Node Security Project (nsp), using [Remarkup table syntax](https://secure.phabricator.com/book/phabricator/article/remarkup/#tables)
+
+See the [nsp](https://github.com/nodesecurity/nsp) website for more information.
---
+++ src/formatter.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = function (err, data, pkgPath) {
+	if (err) {
+		return 'Debug output: ' + JSON.stringify(Buffer.isBuffer(data) ? data.toString() : data) + '\n' + err;
+	}
+
+	var pipeWrap = function (items) {
+		return '|' + items.join('|') + '|';
+	};
+
+	var rows = [];
+	rows.push( pipeWrap( ['Name', 'Installed', 'Patched', 'Include Path', 'More Info'] ) );
+
+	data.forEach( function (finding) {
+		rows.push(
+			pipeWrap(
+				[finding.module, finding.version, finding.patched_versions === '<0.0.0' ? 'None' : finding.patched_versions, finding.path.join(' > '), finding.advisory]
+			)
+		);
+	});
+
+	return rows.join('\n');
+};
