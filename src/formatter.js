3d459bb8bf1e633cc0285fb202830f8d7fca3a45
Darian Anthony Patrick <dpatrick@wikimedia.org>
2016-07-15 17:00:11 -0700

    Add severity display

--- src/formatter.js
+++ src/formatter.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var cvss = require('cvss');
+
 module.exports = function (err, data, pkgPath) {
 	if (err) {
 		return 'Debug output: ' + JSON.stringify(Buffer.isBuffer(data) ? data.toString() : data) + '\n' + err;
@@ -9,14 +11,16 @@ module.exports = function (err, data, pkgPath) {
 		return '|' + items.join('|') + '|';
 	};
 
+	var header = ['Severity', 'Name', 'Installed', 'Patched', 'Include Path', 'More Info'];
+
 	var rows = [];
-	rows.push( pipeWrap( ['Name', 'Installed', 'Patched', 'Include Path', 'More Info'] ) );
-	rows.push( pipeWrap( ['--', '--', '--', '--', '--'] ) );
+	rows.push( pipeWrap(header) );
+	rows.push( pipeWrap( Array(header.length).fill('--') ) );
 
 	data.forEach( function (finding) {
 		rows.push(
 			pipeWrap(
-				[finding.module, finding.version, finding.patched_versions === '<0.0.0' ? 'None' : finding.patched_versions, finding.path.join(' > '), finding.advisory]
+				[cvss.getRating(finding.cvss_score), finding.module, finding.version, finding.patched_versions === '<0.0.0' ? 'None' : finding.patched_versions, finding.path.join(' > '), finding.advisory]
 			)
 		);
 	});
