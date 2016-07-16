d68bbb0d4f05cf4edefacc4750a43a5749c2c81b
Darian Anthony Patrick <dpatrick@wikimedia.org>
2016-07-15 17:45:45 -0700

    Add title and shorten advisory

--- src/formatter.js
+++ src/formatter.js
@@ -11,16 +11,17 @@ module.exports = function (err, data, pkgPath) {
 		return '|' + items.join('|') + '|';
 	};
 
-	var header = ['Severity', 'Name', 'Installed', 'Patched', 'Include Path', 'More Info'];
+	var header = ['Severity', 'Title', 'Module', 'Installed', 'Patched', 'Include Path', 'More Info'];
 
 	var rows = [];
 	rows.push( pipeWrap(header) );
 	rows.push( pipeWrap( Array(header.length).fill('--') ) );
 
 	data.forEach( function (finding) {
+		var advisory_number = finding.advisory.substr(finding.advisory.lastIndexOf('/'));
 		rows.push(
 			pipeWrap(
-				[cvss.getRating(finding.cvss_score), finding.module, finding.version, finding.patched_versions === '<0.0.0' ? 'None' : finding.patched_versions, finding.path.join(' > '), finding.advisory]
+				[cvss.getRating(finding.cvss_score), finding.title, finding.module, finding.version, finding.patched_versions === '<1.0.0' ? 'None' : finding.patched_versions, '{nav ' + finding.path.join(' > ') + '}', '[[' + finding.advisory + '|nspa' + advisory_number + ']]']
 			)
 		);
 	});
