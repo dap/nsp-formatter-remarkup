583712b047124b039ac8c3933a28a875def54215
Darian Anthony Patrick <dpatrick@wikimedia.org>
2017-10-23 17:19:15 -0700

    Correct formatting bug when version contains pipe

--- src/formatter.js
+++ src/formatter.js
@@ -4,25 +4,46 @@ var cvss = require('cvss');
 
 module.exports = function (err, data, pkgPath) {
 	if (err) {
-		return 'Debug output: ' + JSON.stringify(Buffer.isBuffer(data) ? data.toString() : data) + '\n' + err;
+		return err;
 	}
 
 	var pipeWrap = function (items) {
 		return '|' + items.join('|') + '|';
 	};
 
-	var header = ['Severity', 'Title', 'Module', 'Installed', 'Patched', 'Include Path', 'More Info'];
+	var header = [
+		'Severity',
+		'Title',
+		'Module',
+		'Installed',
+		'Patched',
+		'Include Path',
+		'More Info'
+	];
 
 	var rows = [];
 	rows.push( pipeWrap(header) );
 	rows.push( pipeWrap( Array(header.length).fill('--') ) );
 
 	data.forEach( function (finding) {
-		var advisory_number = finding.advisory.substr(finding.advisory.lastIndexOf('/'));
+		var advisory_number
+			= finding.advisory.substr(finding.advisory.lastIndexOf('/'));
+
+		var patched_versions
+			= finding.patched_versions === '<0.0.0'
+				? 'None'
+				: finding.patched_versions.replace(/\|\|/g, 'OR');
+
 		rows.push(
-			pipeWrap(
-				[cvss.getRating(finding.cvss_score), finding.title, finding.module, finding.version, finding.patched_versions === '<1.0.0' ? 'None' : finding.patched_versions, '{nav ' + finding.path.join(' > ') + '}', '[[' + finding.advisory + '|nspa' + advisory_number + ']]']
-			)
+			pipeWrap([
+				cvss.getRating(finding.cvss_score),
+				finding.title,
+				finding.module,
+				finding.version,
+				patched_versions,
+				'{nav ' + finding.path.join(' > ') + '}',
+				'[[' + finding.advisory + '|nspa' + advisory_number + ']]'
+			])
 		);
 	});
 
